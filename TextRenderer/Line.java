25c09c1c107795ab1077e87b7676336a0b1b0b3c
Ccindy0171 <58992827+Ccindy0171@users.noreply.github.com>
2021-10-07 13:17:03 -0400

    Update Line.java

--- TextRenderer/Line.java
+++ TextRenderer/Line.java
@@ -3,7 +3,8 @@ package TextRenderer;
 import java.util.ArrayList;
 
 public class Line implements RenderObject {
-  int x1, y1, x2, y2;
+  public int x1, y1, x2, y2;
+  int _x1, _y1, _x2, _y2;
   int layer;
   boolean vertical_first;
 
@@ -12,64 +13,51 @@ public class Line implements RenderObject {
   }
 
   public Line(int x1, int y1, int x2, int y2, int layer, boolean vertical_first) {
-    if (x1 > x2){
-      this.x1 = x2;
-      this.y1 = y2;
-      this.x2 = x1;
-      this.y2 = y1;
-    } else{
-      this.x1 = x1;
-      this.y1 = y1;
-      this.x2 = x2;
-      this.y2 = y2;
-    }
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
 
     this.layer = layer;
     this.vertical_first = vertical_first;
   }
 
   public Pixel[] draw() {
-    // Needs to decide whether to go up first or horizontal first
-    // I don't think diagonal is possible
     
-    ArrayList<Pixel> arr = new ArrayList<Pixel>();   
-   
+    ArrayList<Pixel> arr = new ArrayList<Pixel>(); 
+
     if (vertical_first){
-      if (y2 > y1){
-        // draw the vertical part first
-        for (int i=0; i<y2-y1; i++){
-          arr.add(new Pixel('|', x1, y1+i, layer));
-        }
-      } else{
-        // draw the vertical part first
-        for (int i=0; i<y1-y2; i++){
-          arr.add(new Pixel('|', x1, y1-i, layer));
-        }
-      }
-      //then horizontal (same no matter which direction line is going)
-      for (int i=0; i<=x2-x1; i++){
-        arr.add(new Pixel('-', x1+i, y2, layer));
-      }
+      draw_vertical(x1, y1, y2, layer, arr);
+      draw_horizontal(x1, x2, y2, layer, arr);
       arr.add(new Pixel('+', x1, y2, layer));
-    }else{
-      // draw horizontal part first
-      for (int i=0; i<=x2-x1; i++){
-        arr.add(new Pixel('-', x1+i, y1, layer));
-      }
-      // then vertical part
-      if (y2 > y1){
-        for (int i=1; i<=y2-y1; i++){
-          arr.add(new Pixel('|', x2, y1+i, layer));
-        }
-      } else{
-        for (int i=0; i<y1-y2; i++){
-          arr.add(new Pixel('|', x2, y1-i, layer));
-        }
-      }
+    } else{
+      draw_horizontal(x1, x2, y1, layer, arr);
+      draw_vertical(x2, y1, y2, layer, arr);
       arr.add(new Pixel('+', x2, y1, layer));
     }
-  
+
     return arr.toArray(new Pixel[arr.size()]);
-    
+  }
+
+  public void draw_vertical(int x, int _y1, int _y2, int layer, ArrayList<Pixel> _arr){
+    if (_y1 > _y2){
+      int new_y1 = _y2;
+      _y2 = _y1;
+      _y1 = new_y1;
+    }
+    for (int i=0; i<_y2 -_y1; i++){
+      _arr.add(new Pixel('|', x, _y1+i, layer));
+    }
+  }
+
+  public void draw_horizontal(int _x1, int _x2, int y, int layer, ArrayList<Pixel> _arr){
+    if (_x1 > _x2){
+      int new_x1 = _x2;
+      _x2 = _x1;
+      _x1 = new_x1;
+    }
+    for (int i=0; i<=_x2 -_x1; i++){
+      _arr.add(new Pixel('-', _x1+i, y, layer));
+    }
   }
 }
