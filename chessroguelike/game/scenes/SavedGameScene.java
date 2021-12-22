835454b0c6dc6e6f76d1028383a84b9c4fc1320d
bistromath25 <71907421+bistromath25@users.noreply.github.com>
2021-12-22 14:26:20 -0500

    Update SavedGameScene.java

--- chessroguelike/game/scenes/SavedGameScene.java
+++ chessroguelike/game/scenes/SavedGameScene.java
@@ -3,13 +3,35 @@ package chessroguelike.game.scenes;
 import chessroguelike.game.Scene;
 import chessroguelike.Menu;
 
-class SavedGameScene extends Scene {
+import java.io.Serializable;
 
+import chessroguelike.textRenderer.*;
+/*
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+*/
+
+class SavedGameScene extends Scene {
+    Scene savedGameScene;
     SavedGameScene(int width, int height, Listener listener) {
         super(width, height, listener);
+        objects.put(new Text("Saved Games", 20), new Position(2, 2));
     }
 
-	public void input(char c) {
-		refreshScreen();
+	public void input(char c) {  // b to go "back" to main menu
+        if (c == 'b') {
+            backToMainMenu();
+        }
+       // else if (c == 's') {  // save game info to .ser file
+            //saveGame();
+        //    backToMainMenu();
+        //}
 	}
+
+    void backToMainMenu() {
+        listener.move(new MenuScene(width, height, listener));
+    }
 }
