3d88c5614d82a3acba7af774cd939bbec5f755ea
bistromath25 <71907421+bistromath25@users.noreply.github.com>
2021-12-22 14:19:02 -0500

    Create GameSavedMsg.java
    This scene played whenever we save a game

---
+++ chessroguelike/game/scenes/GameSavedMsg.java
@@ -0,0 +1,31 @@
+package chessroguelike.game.scenes;
+
+import chessroguelike.game.Scene;
+import chessroguelike.Menu;
+import chessroguelike.textRenderer.*;
+
+public class GameSavedMsg extends Scene {
+    Menu menu;
+    public GameSavedMsg(int width, int height, Listener listener) {
+        super(width, height, listener);
+		Text t = new Text("Game has been saved - press 'l' to load saved games, or 'b' to go back to main menu.", 25);
+        objects.put(t, new Position(2, 2));
+    }
+
+    void backToMainMenu() {
+        listener.move(new MenuScene(width, height, listener));
+    }
+
+	public void input(char c) {
+		if (c == 'b') {
+            backToMainMenu();
+        } 
+        if (c == 'l') {
+            toGameScene();
+        }
+    }
+
+    void toGameScene() {
+        listener.move(new GameScene(width, height, listener));
+    }
+}
