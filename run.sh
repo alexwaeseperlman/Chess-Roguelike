62b05a5852b320a4cd4d84a1b918a18eff1d6fbd
Alex Waese-Perlman <squishybanana04@gmail.com>
2021-12-23 07:33:20 -0500

    document run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
+#!/bin/sh
+
+# Usage:
+# The first parameter is the file you want to compile for
+# Second parameter is the class+package name.
+# e.g. ./run.sh Main.java Main will run the full game
+# ./run.sh tests/UserInterface.java tests.UserInterface will run the user interface test
+
 FILE="${1:-Main.java}"
 CLASS="${2:-Main}"
 echo $FILE $CLASS
