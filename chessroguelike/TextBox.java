17b573debf44fe9d02af474f504c6195aefffc82
Alex Waese-Perlman <squishybanana04@gmail.com>
2021-12-23 07:12:25 -0500

    Add text box

---
+++ chessroguelike/TextBox.java
@@ -0,0 +1,71 @@
+package chessroguelike;
+import chessroguelike.textRenderer.*;
+
+/**
+ * A {@link RenderObject} that allows a user to enter text
+ **/
+public class TextBox extends Renderer {
+    /**
+     * The {@link Listener} object is used to receive inputs from a text box.
+     **/
+    public static interface Listener {
+        /**
+         * @param text The content that the user entered
+         **/
+        public void submitted(String text);
+    }
+	Rect box;
+    Text textDisplay;
+	
+	int width, height;
+	String text = "";
+
+    Listener l;
+
+    /**
+     * Construct a {@link TextBox} with given width and height.
+     * @param width The menu's width
+     * @param height The menu's height
+     * @param l A listener object for the constructed menu
+     **/
+	public TextBox(int width, int height, Listener l) {
+		super(width, height);
+        this.l = l;
+        
+		box = new Rect(width-1, height-1, 1000);
+		box.backgroundChar = ' ';
+		textDisplay = new Text(text + '|', width-2, 1001);
+		this.objects.put(box, new Position(0, 0));
+		this.objects.put(textDisplay, new Position(1, 1));
+        update();
+	}
+
+    /**
+     * Update the appearance of the text box to take into
+     * account the currently selected inputted text
+     **/
+    void update() {
+		textDisplay.content = text;
+    }
+    /**
+     * Input a character to this textbox while handling delete key presses.
+     **/
+    public void type(char input) {
+		if (input == 13) {
+			submit();
+			return;
+		}
+		if (input == 127) {
+			text = text.substring(0, text.length()-1);
+		}
+		else text += input;
+		textDisplay.content = text + '|';
+    }
+
+    /**
+     * Calls the event {@link Listener} for this text box
+     **/
+    public void submit() {
+		l.submitted(text);
+    }
+}
