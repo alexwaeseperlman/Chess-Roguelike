22048b60c458e5e1522f484de166929405e1d76a
bistromath25 <71907421+bistromath25@users.noreply.github.com>
2021-12-30 13:14:26 -0500

    Update InstructionsScene.java

--- chessroguelike/game/scenes/InstructionsScene.java
+++ chessroguelike/game/scenes/InstructionsScene.java
@@ -7,7 +7,7 @@ class InstructionsScene extends Scene {
 
 	InstructionsScene(int width, int height, Listener listener) {
 		super(width, height, listener);
-		objects.put(new Text("Welcome to ChessRogueLike! ♔ ♘\n\nHere you will play as chess piece, capturing enemies and ascending up through the ranks - but beware! One unwary step may lead to your downfall.\n\nUse H J K L to cycle through your possible moves, which will be shown on the board - a red highlight means you're attacking an enemy!\n\nYou can save your progress and load games from the Saved Games menu - sort by date modified, name, or levels completed.\n\nGood luck, adventurer! May your vision stay sharp, and your heart true. In case you forgot, the Knight moves in an 'L' shape...", 50), new Position(2, 2));
+		objects.put(new Text("Welcome to ChessRogueLike! ♔ ♘\n\nHere you will play as chess piece, capturing enemies and ascending up through the ranks - but beware! One unwary step may lead to your downfall.\n\nUse H J K L to cycle through your possible moves, which will be shown on the board - a red highlight means you're attacking an enemy!\n\nYou can save your progress and load games from the Saved Games menu - sort by date modified, name, or levels completed.\n\nGood luck, adventurer! May your vision stay sharp, and your heart true. In case you forgot, the Knight moves in an 'L' shape...\n\n\nPress 'b' to return to the main menu.", 50), new Position(2, 2));
 	}
 
 	public void input(char c) {
