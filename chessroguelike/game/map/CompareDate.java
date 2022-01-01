9ac6fa6fce3ed7922d49defa8ec349407579c82c
bistromath25 <71907421+bistromath25@users.noreply.github.com>
2021-12-31 19:49:31 -0500

    Update CompareDate.java

--- chessroguelike/game/map/CompareDate.java
+++ chessroguelike/game/map/CompareDate.java
@@ -5,8 +5,12 @@ import java.util.Comparator;
 public class CompareDate implements Comparator<SavedGame> {
     /**
      * Sort SavedGame objects by date saved
+     * Games saved on the same date should be ordered alphabetically
      **/
-    public int compare(SavedGame A, SavedGame B) {
+    if (A.lastModified.equals(B.lastModified)) {
+            return A.name.compareTo(B.name);
+        }
+    else {
         return A.lastModified.compareTo(B.lastModified);
     }
 }
